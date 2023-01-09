e9b0e7e51ffa6229fdd0ad63a58969c6dcbbc04b
justdreamers <2534405346@qq.com>
2023-01-09 21:02:04 +0800

    。

---
+++ test_1_9/test_1_9/test.c
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int c = 30;
+//	int d = 40;
+//	int* arr[4] = { &a,&b,&c,&d };
+//	int i = 0;
+//	for (i = 0;i < 4;i++)
+//	{
+//		printf("%d \n", *arr[i]);
+//	}
+//	return  0;
+//}
+int main()
+{
+	int arr1[] = { 1,2,3,4,5 };
+	int arr2[] = { 2,3,4,5,6 };
+	int arr3[] = { 3,4,5,6,7 };
+	int* parr[] = { arr1,arr2,arr3 };
+	int i = 0;
+	for (i = 0;i < 3;i++)
+	{
+		int j = 0;
+		for (j = 0;j < 4;j++)
+		{
+			printf("%d ",*(parr[i] + j));
+		}
+		printf("\n");
+	}
+	return 0;
+}
