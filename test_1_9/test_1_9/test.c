3f31a5adb7ce317be879d95bb151a6b8ef683e54
justdreamers <2534405346@qq.com>
2023-01-10 17:03:40 +0800

    。

--- test_1_9/test_1_9/test.c
+++ test_1_9/test_1_9/test.c
@@ -14,22 +14,68 @@
 //	}
 //	return  0;
 //}
-int main()
+//int main()
+//{
+//	int arr1[] = { 1,2,3,4,5 };
+//	int arr2[] = { 2,3,4,5,6 };
+//	int arr3[] = { 3,4,5,6,7 };
+//	int* parr[] = { arr1,arr2,arr3 };
+//	int i = 0;
+//	for (i = 0;i < 3;i++)
+//	{
+//		int j = 0;
+//		for (j = 0;j < 4;j++)
+//		{
+//			printf("%d ",*(parr[i] + j));
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int (*pa)[10]=&arr;
+//	int i = 0;
+//	for (i = 0;i < 10;i++)
+//	{
+//		printf("%d ", (*pa)[i]);
+//	}
+//	return 0;
+//}
+void print1(int arr[3][5],int x ,int y )
 {
-	int arr1[] = { 1,2,3,4,5 };
-	int arr2[] = { 2,3,4,5,6 };
-	int arr3[] = { 3,4,5,6,7 };
-	int* parr[] = { arr1,arr2,arr3 };
 	int i = 0;
-	for (i = 0;i < 3;i++)
+	int j = 0;
+	for (i = 0;i < x;i++)
 	{
-		int j = 0;
-		for (j = 0;j < 4;j++)
+		for (j = 0;j < y;j++)
 		{
-			printf("%d ",*(parr[i] + j));
+			printf("%d ", arr[i][j]);
 		}
 		printf("\n");
 	}
+}
+void print2(int(*p)[5],int x,int y)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0;i < x;i++)
+	{
+		for (j = 0;j < y;j++)
+		{
+		//	printf("%d ",*(*(p + i) + j));
+		//	printf("%d ", p[i][j]);
+		//	printf("%d ",*(p[i])+j);
+			printf("%d ", (*(p+i))[j]); 
+		}
+		printf("\n");
+	}
+}
+int main()
+{
+	int arr[3][5] = { {1,2,3,4,5},{2,3,4,5,6},{3,4,5,6,7} };
+	//print1(arr, 3, 5);
+	print2(arr, 3, 5);
 	return 0;
 }
-//这时测试用的语句
